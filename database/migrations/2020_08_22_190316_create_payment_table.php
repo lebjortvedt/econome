942ae4a86aefde29d220a34507506965ffa6dc6f
Lars-Eivind Bjørtvedt <larseivind.bjoertvedt@gmail.com>
2020-08-22 21:12:22 +0200

    Added DB migrations

---
+++ database/migrations/2020_08_22_190316_create_payment_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment', function (Blueprint $table) {
+            $table->id();
+            $table->integer('payment_category_id');
+            $table->integer('user_day_id');
+            $table->integer('vendor_id');
+            $table->decimal('amount', 5, 2);
+            $table->tinyinteget('subscrition');
+            $table->softDeletes();
+            $table->timestamps();        
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment');
+    }
+}
